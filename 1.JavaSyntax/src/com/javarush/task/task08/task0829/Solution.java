64ca72d58b080c9547a4ba369e3f27af980e66f9
Igor <kaze250t@gmail.com>
2017-12-21 17:20:11 +0300

    commit1

--- 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /* 
 Модернизация ПО
@@ -14,21 +16,20 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //list of addresses
-        List<String> addresses = new ArrayList<String>();
+        //Map of addresses
+        Map<String, String> addresses = new HashMap<>();
         while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
             String family = reader.readLine();
-            if (family.isEmpty()) break;
-
-            addresses.add(family);
+            addresses.put(city, family);
         }
 
-        //read home number
-        int houseNumber = Integer.parseInt(reader.readLine());
+        String city2 = reader.readLine();
 
-        if (0 <= houseNumber && houseNumber < addresses.size()) {
-            String familySecondName = addresses.get(houseNumber);
-            System.out.println(familySecondName);
+        for (Map.Entry<String, String> entry: addresses.entrySet()) {
+            if (entry.getKey().equals(city2))
+                System.out.println(entry.getValue());
         }
     }
 }
