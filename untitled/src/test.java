899ac27af0661970699b7d8378fa48fe7ef51824
Zorych <kaze250t@gmail.com>
2018-07-04 15:01:28 +0300

    Коммит_1 04.07

---
+++ untitled/src/test.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = a + b;
+        System.out.println(c);
+    }
+}
